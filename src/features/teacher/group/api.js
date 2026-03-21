eb9c2f45c8ecb7dd0176f846933ba1ba4dbc1705
NAPHthalene130 <1301537275@qq.com>
2026-03-21 16:28:15 +0800

    feat:初步实现小组创建与新增成员功能

--- src/features/teacher/group/api.js
+++ src/features/teacher/group/api.js
@@ -1,5 +1,17 @@
-import { get } from '../../../shared/api/httpClient.js';
+import { get, post } from '../../../shared/api/httpClient.js';
 
 export async function getTeacherGroups(teacherID) {
   return get('/group/list', { teacherID });
+}
+
+export async function createTeacherGroup(name) {
+  return post('/group/create', { name });
+}
+
+export async function getGroupStudents(groupID) {
+  return get('/group/students', { groupID });
+}
+
+export async function addGroupStudent(groupID, studentID) {
+  return post('/group/add-student', { groupID, studentID });
 }
